57d3e6b2c88ca3d24d4a6ee3140d7b3fc8252839
Kim Taebum <ddino99@naver.com>
2025-08-20 14:32:09 +0900

    20240820

--- Assn5/Assn5.cpp
+++ Assn5/Assn5.cpp
@@ -19,11 +19,18 @@ public:
 class HashMap
 {
 public:
-    Node* arr[1024];
+    static const int HASH_SIZE = 1024;
+    Node* arr[HASH_SIZE];
 
 public:
+    HashMap() {
+        for (int i = 0; i < HASH_SIZE; i++) {
+            arr[i] = nullptr;
+        }
+    }
+
     int hash(int v) {
-        return v;
+        return (v * 8321648 + 213846) % HASH_SIZE;
     }
 
     void insert(int k, int v) {
@@ -31,47 +38,95 @@ public:
         int idx = hash(k);
         if (arr[idx] == nullptr) {
             arr[idx] = node;
+            return;
         }
-        else {
-            Node* now = arr[idx];
-            while (now->next) {
-                now = now->next;
-            }
 
-            now->next = node;
+        Node* now = arr[idx];
+        while (now->next) {
+            now = now->next;
         }
+
+        now->next = node;
+        node->prev = now;
     }
 
     void erase(int k) {
         int idx = hash(k);
         if (arr[idx] == nullptr) {
             cout << "No Value" << endl;
+            return;
         }
-        else {
-            Node* now = arr[idx];
-            while (now) {
-                if (now->key == k) {
-                    if (now->prev) {
-                        now->prev->next = now->next;
+
+        Node* now = arr[idx];
+        while (now) {
+            if (now->key == k) {
+                if (now->prev == nullptr) {
+                    arr[idx] = now->next;
+                    if (now->next) {
+                        now->next->prev = nullptr;
                     }
+                }
+                else {
+                    now->prev->next = now->next;
                     if (now->next) {
                         now->next->prev = now->prev;
                     }
-
-                    delete now;
-                    return;
                 }
 
-                now = now->next;
+                delete now;
+                return;
             }
 
-            cout << "No Value" << endl;
+            now = now->next;
         }
+
+        cout << "No Value" << endl;
+    }
+
+    void showAll() {
+        for (int i = 0; i < HASH_SIZE; i++) {
+            if (arr[i]) {
+                Node* now = arr[i];
+                while (now) {
+                    cout << "key: " << now->key << " value: " << now->value << endl;
+                    now = now->next;
+                }
+            }
+        }
+    }
+
+    int find(int k) {
+        int idx = hash(k);
+        if (arr[idx] == nullptr) {
+            cout << "No Value" << endl;
+            return 0;
+        }
+        
+        Node* now = arr[idx];
+        while (now) {
+            if (now->key == k) {
+                return now->value;
+            }
+
+            now = now->next;
+        }
+
+        cout << "No Value" << endl;
+        return 0;
     }
 };
 
 int main()
 {
-    
+    HashMap* hm = new HashMap();
+    hm->insert(5, 10);
+    hm->insert(10, 15);
+    hm->insert(20, 25);
+    hm->insert(30, 35);
+    hm->erase(20);
+
+    hm->showAll();
+    cout << hm->find(10) << endl;;
+    cout << hm->find(20) << endl;
 }
 
